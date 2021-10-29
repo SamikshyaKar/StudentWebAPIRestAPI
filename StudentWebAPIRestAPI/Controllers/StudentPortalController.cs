ffd12d547f11a4831d77139bae5fdf1e888ce973
Samikshya <write2samikshyakar@gmail.com>
2021-10-29 17:36:06 +0530

    Add project files.

---
+++ StudentWebAPIRestAPI/Controllers/StudentPortalController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudentWebAPIRestAPI.Controllers
+{
+
+    [ApiController]
+    [Route("api/[controller]")]
+    public class StudentPortalController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ StudentWebAPIRestAPI/Models/StudentPortalDetails.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudentWebAPIRestAPI.Models
+{
+    public class StudentPortalDetails
+    {
+        public int StudentID { get; set; }
+        public int StudentAge { get; set; }
+        public string StudentName { get; set; }
+        public string StudentAdderss { get; set; }
+
+
+
+    }
+}
